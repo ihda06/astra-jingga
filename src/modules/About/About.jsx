8bd165752270d07496f2eb2888b26a7f66a51ab6
ihda06 <ihdaanwari5@gmail.com>
2023-10-25 19:40:16 +0700

    fix: image h-full problem

--- src/modules/About/About.jsx
+++ src/modules/About/About.jsx
@@ -9,11 +9,12 @@ export default function About({}) {
   return (
     <div className="flex flex-col gap-8 lg:flex-row py-14 lg:px-24 px-12">
       <div className="basis-2/5 flex justify-center w-full">
+       
         <motion.div
           initial={{ x: -50, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ type: "spring", duration: 0.5 }}
-          className="rounded-lg relative h-full w-64 overflow-hidden shadow-[15px_15px_16px_0px_rgba(0,0,0,0.25)]"
+          className="rounded-lg relative h-[304px] w-64 overflow-hidden shadow-[15px_15px_16px_0px_rgba(0,0,0,0.25)]"
         >
           <Image
             fill
@@ -24,14 +25,11 @@ export default function About({}) {
           ></Image>
         </motion.div>
       </div>
-      <motion.div
-        initial={{ x: 50, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ type: "spring", duration: 0.5 }}
-        className="basis-3/5 max-w-[800px] flex flex-col gap-5"
+      <div
+        className="basis-3/5 lg:max-w-[800px] flex flex-col  gap-5"
       >
         <AboutContent></AboutContent>
-      </motion.div>
+      </div>
     </div>
   );
 }
